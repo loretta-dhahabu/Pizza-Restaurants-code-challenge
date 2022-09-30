706318f256673d568a9590af0da2b7ac813994ae
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-30 16:44:50 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :restaurant_pizzas
-  resources :pizzas
-  resources :restaurants
+  resources :restaurant_pizzas, only[:index, :create]
+  resources :pizzas, only[:index, :show, :create, :update]
+  resources :restaurants, only[:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
