368ccdeeb9042b04d4b29552ead97eb1541c358a
loretta dhahabu <loreydjefwa@gmail.com>
2022-10-01 16:02:17 +0300

    create components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from './component/Header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Router>
+        <Header />
+        <Routes>
+          {/* <Route exact path="/" element={<Home />}></Route> */}
+          {/* <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/meals" element={<FoodsList />}></Route>
+          <Route exact path="/meals/:id" element={<SingleMeal />}></Route> */}
+          {/* <Route exact path="/meals/:id/edit" element="<Home/>" /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
