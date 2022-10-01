b8ba4a7c45fd7fe5a279a9642d553045a57cb78a
loretta dhahabu <loreydjefwa@gmail.com>
2022-10-01 13:49:51 +0300

    create a restaurant pizza

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,19 @@
 class RestaurantPizzasController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_error
+
+    # POST /restaurant_pizzas
+    def create
+        restaurant_pizza = RestaurantPizza.create!(restaurant_pizza_params)
+        render json: restaurant_pizza.pizza,  status: :created
+    end
+
+    private
+
+    def restaurant_pizza_params
+        params.permit(:price, :pizza_id, :restaurant_id)
+    end
+
+    def unprocessable_entity_error(exception)
+        render json: {error: "errors: exception.record.errors.full_messages"}, status: :unprocessable_entity
+    end
 end
