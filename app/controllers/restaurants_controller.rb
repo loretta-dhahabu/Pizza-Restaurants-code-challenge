7ecf60fe7ac2d85da8496b7bf743c85761868a12
loretta dhahabu <loreydjefwa@gmail.com>
2022-10-01 11:37:28 +0300

    get all restaurants and get each restaurant by params

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,14 @@
 class RestaurantsController < ApplicationController
+    
+    #GET /restaurants
+    def index
+        restaurants = Restaurant.all
+        render json: restaurants
+    end
+
+    #GET /restaurants/:id
+    def show
+        restaurant = Restaurant.find_by(id: params[:id])
+        render json: restaurant
+    end
 end
