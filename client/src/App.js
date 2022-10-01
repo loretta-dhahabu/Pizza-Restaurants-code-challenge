a8f8a99592825f2fdb064a68f7b8348945289ad9
loretta dhahabu <loreydjefwa@gmail.com>
2022-10-01 17:41:43 +0300

    create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
 import './App.css';
 import Header from './component/Header';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
-
+import Home from './component/Home';
+import RestaurantList from './component/RestaurantList';
+import PizzaList from './component/PizzaList';
+import Restaurant from './component/Restaurant'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
+        {/* <Home /> */}
         <Routes>
-          {/* <Route exact path="/" element={<Home />}></Route> */}
-          {/* <Route exact path="/about" element={<About />}></Route>
-          <Route exact path="/meals" element={<FoodsList />}></Route>
-          <Route exact path="/meals/:id" element={<SingleMeal />}></Route> */}
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/restaurants" element={<RestaurantList />}></Route>
+          <Route path="/pizzas" element={<PizzaList />}></Route>
+          <Route path="/restaurants/:id" element={<Restaurant />}></Route>
           {/* <Route exact path="/meals/:id/edit" element="<Home/>" /> */}
         </Routes>
       </Router>
---
+++ client/src/component/PizzaList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PizzaList() {
+  return (
+    <div>PizzaList</div>
+  )
+}
+
+export default PizzaList;
