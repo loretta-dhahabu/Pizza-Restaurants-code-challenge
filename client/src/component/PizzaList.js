baea9a04d2b506a53e9dbd8f7ae04286a1738170
loretta dhahabu <loreydjefwa@gmail.com>
2022-10-03 20:11:40 +0300

    get all pizzas

--- client/src/component/PizzaList.js
+++ client/src/component/PizzaList.js
@@ -3,11 +3,54 @@ import React, { useEffect, useState } from 'react'
 
 function PizzaList ()
 {
-  const [ pizzas, SetPizza ] = useState( [] )
+  const [ pizzas, SetPizzas ] = useState( [] )
   
- 
+  useEffect( () =>
+  {
+    fetch( '/pizzas' )
+      .then( ( response ) =>response.json())
+      .then( ( data ) =>
+      {
+        console.log( data );
+        SetPizzas(data)
+    })
+  }, [] )
+  
+  let getPizzas = pizzas.map( ( pizza ) =>
+  {
+    return (
+      <div className="restbody" key={pizza.id}>
+        <div className="container bg-darksalmon">
+          <div className="card restcards">
+            <img
+              src={pizza.image}
+              className="card-img-top restimages"
+              alt={pizza.name}
+            />
+            <div className="card-body">
+              <h5 className="card-title text-center">
+                Pizza-Name: <em>{pizza.name}</em>
+              </h5><br></br>
+              <h4>Description</h4>
+              <p className="card-text text-center">{pizza.description}</p>
+              <h4>Ingredients</h4>
+              <p className="card-text text-center">{pizza.ingredients}</p>
+              <div className="pizza-details">
+                {/* <Link
+                  to={`/pizzas/${pizza.id}`}
+                  onClick={() => <pizza key={pizza.id} />}
+                >
+                  <button className="viewBtn">View More</button>
+                </Link> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  })
   return (
-    <div>PizzaList</div>
+    <div>{getPizzas}</div>
   )
 }
 
